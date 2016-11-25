24813a17fcc7dc5d1dd0971a103b15737d7ae255
nicepat <nicepat@gmail.com>
2016-11-24 16:00:54 -0800

    moving to Web Service

--- sample1/test.py
+++ sample1/test.py
@@ -4,6 +4,24 @@ import string
 
 import cherrypy
 
+@cherrypy.expose
+class StringGeneratorWebService(object):
+    
+    @cherrypy.tools.accept(media='text/plain')
+    def GET(self):
+        return cherrypy.session['mystring']
+    
+    def POST(self, length=8):
+        some_string = ''.join(random.sample(string.hexdigits, int(length)))
+        cherrypy.session['mystring'] = some_string
+        return some_string
+    
+    def PUT(self, another_string):
+        cherrypy.session['mystring'] = another_string
+        
+    def DELETE(self):
+        cherrypy.session.pop('mystring', None)
+
 class StringGenerator(object):
     @cherrypy.expose
     def index(self):
@@ -32,13 +50,16 @@ class StringGenerator(object):
 if __name__ == '__main__':
     conf = {
         '/': {
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
             'tools.sessions.on': True,
-            'tools.staticdir.root': os.path.abspath(os.getcwd())
-            },
-        '/static': {
-            'tools.staticdir.on': True,
-            'tools.staticdir.dir': '.public'
+            'tools.response_headers.on': True,
+            'tools.response_headers.headers': [('Content-Type', 'text/plan')],
+#             'tools.staticdir.root': os.path.abspath(os.getcwd())
+#             },
+#         '/static': {
+#             'tools.staticdir.on': True,
+#             'tools.staticdir.dir': '.public'
             }
         }
-    cherrypy.quickstart(StringGenerator(),'/', conf)
+    cherrypy.quickstart(StringGeneratorWebService(),'/', conf)
     
